c74ae83eadf1332cbfd9a80eff3e256eb77afe10
煜儿 <15201139181@163.com>
2020-04-24 14:09:27 +0800

    pro add  send code module

--- src/api/user.js
+++ src/api/user.js
@@ -1,9 +1,29 @@
 import request from "@/utils/request";
 
+/* 
+登录请求接口
+@params  
+data: mobile 手机号
+      code  验证码
+*/
 export const login = data => {
   return request({
     method: "POST",
     url: "/app/v1_0/authorizations",
     data
+  });
+};
+
+/* 
+发送验证码接口
+@params  
+data: mobile 手机号
+接口请求--每分钟一次
+*/
+
+export const send = mobile => {
+  return request({
+    method: "GET",
+    url: `/app/v1_0/sms/codes/${mobile}`
   });
 };
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="login-container">
     <van-nav-bar title="登录" class="page-nav-bar" />
-    <van-form @submit="onSubmit">
+    <van-form @submit="onSubmit" ref="loginInfo">
       <van-field
         v-model="user.mobile"
-        name="用户名"
+        name="mobile"
         placeholder="请输入手机号"
         :rules="userFormRules.mobile"
         type="digit"
@@ -15,7 +15,7 @@
       <van-field
         v-model="user.code"
         left-icon="smile-o"
-        name="密码"
+        name="code"
         placeholder="请输入验证码"
         :rules="userFormRules.code"
         type="digit"
@@ -23,7 +23,21 @@
       >
         <i class="iconfont- iconyanzhengma" slot="left-icon"></i>
         <template #button>
-          <van-button size="small" round type="default" class="sendBtn">发送验证码</van-button>
+          <van-count-down
+            v-if="showCodeStatus"
+            @finish="resetTime"
+            :time="time"
+            format="ss s后重新发送"
+          />
+          <van-button
+            v-else
+            size="small"
+            native-type="button"
+            round
+            type="default"
+            class="sendBtn"
+            @click="onSend"
+          >发送验证码</van-button>
         </template>
       </van-field>
       <div style="margin: 16px;">
@@ -33,14 +47,14 @@
   </div>
 </template>
 <script>
-import { login } from '@/api/user'
+import { login, send } from '@/api/user'
 export default {
   name: 'loginIndex',
   data() {
     return {
       user: {
-        mobile: '13911111111',
-        code: '246810'
+        mobile: '15201139181',
+        code: ''
       },
       userFormRules: {
         mobile: [
@@ -51,10 +65,13 @@ export default {
           { required: true, message: '请输入验证码' },
           { pattern: /^\d{6}$/, message: '请填写正确的验证码' }
         ]
-      }
+      },
+      showCodeStatus: false /* 倒计时状态初始值 - false--隐藏   true--显示 */,
+      time: 60 * 1000 /* 倒计时时间 */
     }
   },
   methods: {
+    // 登录前手机及验证码格式验证
     async onSubmit() {
       const user = this.user
       this.$toast.loading({
@@ -72,6 +89,33 @@ export default {
           this.$toast.fail('登录失败，请稍候重试')
         }
       }
+    },
+    // 发送验证码
+    async onSend() {
+      try {
+        await this.$refs.loginInfo.validate('mobile')
+      } catch (error) {
+        console.log(error)
+        return false
+      }
+      // 验证码倒计时状态显示
+      this.showCodeStatus = true
+      try {
+        await send(this.user.mobile)
+        this.$toast.success('验证码已发送')
+      } catch (error) {
+        // 发送失败----验证码倒计时状态隐藏
+        this.showCodeStatus = false
+        if (error.response.status === 429) {
+          this.$toast.fail('发送频繁，请稍候重试')
+        } else {
+          this.$toast.fail('发送验证码失败，请稍候重试')
+        }
+      }
+    },
+    // 验证码倒计时状态隐藏
+    resetTime() {
+      this.showCodeStatus = false
     }
   }
 }
